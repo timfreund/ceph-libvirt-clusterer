5955b8243298030e3f32a9c9c5c77dd79b568602
Tim Freund <tim@freunds.net>
2015-07-19 03:35:12 -0400

    Cluster.create_volume and Cluster.duplicate_volume
    create_volume creates a blank volume in the provided pool or the first
    available pool in the host's pool list.
    
    duplicate_volume duplicates a volume in the same pool as the source
    volume.

--- cephlvc/__init__.py
+++ cephlvc/__init__.py
@@ -14,7 +14,6 @@ class Cluster(object):
         etree.find("uuid").text = str(new_id)
 
         template_volume_path = etree.find('*/disk/source').attrib['file']
-        
 
     def add_volume_to_domain(self, domain):
         # domain.attachDeviceFlags
@@ -23,9 +22,41 @@ class Cluster(object):
     def detach_volume(self, domain, volume):
         pass
 
+    def create_volume(self, name, size, pool=None):
+        if not pool:
+            pool = self.virtcon.listAllStoragePools()[0]
+        xml = """
+        <volume type='file'>
+          <name>%s</name>
+          <capacity unit='bytes'>%d</capacity>
+          <allocation unit='bytes'>0</allocation>
+          <target>
+            <format type='qcow2'/>
+          </target>
+        </volume>
+        """ % (name, size * 1024 * 1024)
+        volume = pool.createXML(xml)
+        return volume
+
     @property
     def domains(self):
         return [d for d in self.virtcon.listAllDomains() if d.name().startswith(self.name)]
+
+    def duplicate_volume(self, volume, new_name):
+        if isinstance(volume, str):
+            if volume.find(os.path.sep) == 0:
+                volume = self.virtcon.storageVolLookupByPath(volume)
+            else:
+                for pool in host.listAllStoragePools():
+                    for v in pool.listAllVolumes():
+                        if v.name() == volume:
+                            volume = v
+                            break
+        template_name = volume.name()
+        pool = volume.storagePoolLookupByVolume()
+        new_xml = volume.XMLDesc().replace(template_name, new_name)
+        new_vol = pool.createXMLFrom(new_xml, volume)
+        return new_vol
 
     def next_domain_name(self):
         return "%s-%02d" % (self.name, len(self.domains))
--- cephlvc/tests/integration/testvolumes.py
+++ cephlvc/tests/integration/testvolumes.py
@@ -1,7 +1,15 @@
 from unittest import TestCase
 import libvirt
 
+from cephlvc import Cluster
+
 class TestVolumeManagement(TestCase):
-    def test_dummy_test(self):
-        vc = libvirt.open()
-        
+    def test_duplicate_volume(self):
+        virtcon = libvirt.open()
+        c = Cluster("integration", None, virtcon)
+        original = c.create_volume("cephlvc-test_duplicate_volume-original.img", 8)
+        duplicate = c.duplicate_volume(original, "cephlvc-test_duplicate_volume-duplicate.img")
+
+        self.assertEquals("cephlvc-test_duplicate_volume-duplicate.img", duplicate.name())
+        self.assertEquals(0, original.delete())
+        self.assertEquals(0, duplicate.delete())
